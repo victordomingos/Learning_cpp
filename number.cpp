7068622f14e212e226f095c51396fd1a144d19bf
Victor Domingos <victordomingos@users.noreply.github.com>
2019-04-09 12:08:35 +0100

    Create number.cpp

---
+++ number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+    cout << "\nPor favor, introduza um numero: ";
+	cin >> n;
+	cout << "Introduziu o numero " << n << "." << endl << endl;
+}
