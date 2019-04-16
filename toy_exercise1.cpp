f5ab88e9fbdbb29c6e3bb5572d4c0aeb1365814e
Victor Domingos <victordomingos@users.noreply.github.com>
2019-04-16 12:44:52 +0100

    Create toy_exercise1.cpp

---
+++ toy_exercise1.cpp
@@ -0,0 +1,26 @@
+/* toy_exercise1.cpp: print a text-mode graph of a given temperature
+
+   Ask the user for an integer number and then display an indicator 
+   like the one below (# means 2 degrees; + means 1 degree):
+  
+     How many degrees (enter an integer number)?
+     ### ### ### ##+ 
+ 
+   2019-04-16
+   Victor Domingos
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\"GRAPHICAL REPRESENTATION OF A CITY'S TEMPERATURE\"" << endl;
+    cout << "(# means 2 degrees; + means 1 degree)" << endl << endl;
+
+    cout << "Please enter the temperature in degrees (an integer number): ";
+
+    for (size_t i = 0; i < length; i++)
+    {
+
+    }
+}
