a028ef3fa1a5f683945b605930e6aef6e7052382
Victor Domingos <victordomingos@users.noreply.github.com>
2019-05-09 16:49:25 +0100

    Update 025-Production2d.cpp

--- 025-Production2d.cpp
+++ 025-Production2d.cpp
@@ -1,10 +1,10 @@
-/* Produção 2 - parte IV
+/* ProduÃ§Ã£o 2 - parte IV
    2019-05-02 Victor Domingos
 
-   Ler 5 números para um array.
-   Escrever numa só linha : 
-   
-   “Há x pares, y ímpares, z maiores que o primeiro, w abaixo da média.”
+   Ler 5 nÃºmeros para um array.
+   Escrever numa sÃ³ linha :
+
+   â€œHÃ¡ x pares, y Ã­mpares, z maiores que o primeiro, w abaixo da mÃ©dia.â€
 */
 
 #include <iostream>
@@ -13,15 +13,16 @@ using namespace std;
 
 int main()
 {
-    int nums[5] = { 0.0 };
-    int nums_sum = 0;
-    int nums_mean = 0.0;
+    float nums[5] = { 0.0 };
+    float nums_sum = 0;
+    float nums_mean = 0.0;
+
     int even_count = 0;
     int odd_count = 0;
     int greater_than_first_count = 0;
     int below_mean_count = 0;
 
-    // Ler 5 números para um array.
+    // Ler 5 nÃºmeros para um array.
     cout << "\nPor favor, introduza 5 numeros (pressione ENTER depois de escrever cada um deles):";
     cout << "\n? ";   cin >> nums[0];
     cout << "? ";     cin >> nums[1];
@@ -29,9 +30,9 @@ int main()
     cout << "? ";     cin >> nums[3];
     cout << "? ";     cin >> nums[4];
 
-    // Calcular a soma de todos os números.
+    // Calcular a soma de todos os nÃºmeros.
     // Contar pares.
-    // Contar ímpares.
+    // Contar Ã­mpares.
     // Contar maiores que o primeiro.
     for (int i = 0; i < 5; i++)
     {
@@ -40,16 +41,16 @@ int main()
         if (nums[i] > nums[0])
             greater_than_first_count++;
 
-        if (nums[i] % 2 == 0)
+        if ((int)nums[i] % 2 == 0)
             even_count++;
         else
             odd_count++;
     }
 
-    // Calcular a média
+    // Calcular a mÃ©dia
     nums_mean = nums_sum / 5;
 
-    // Contar números abaixo da média
+    // Contar nÃºmeros abaixo da mÃ©dia
 
     for (int i = 0; i < 5; i++)
     {
