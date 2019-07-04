54b6a28d5cef76f9eb7a308a741959d793deb1f6
victordomingos <editor.arcosonline@gmail.com>
2019-07-04 16:40:22 +0100

    01_variables_and_pointers.c

---
+++ Data_Structures/01_variables_and_pointers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x = 20;
+    int *p = NULL;
+
+    p = &x;
+
+    printf("\nValor de X: \t\t\t\t%d", x);
+    printf("\nEndereço de mem. da var. X: \t\t%p", p);
+    
+
+    printf("\nEndereço de mem. da variável apontada:\t%d", *p);
+    printf("\nEndereço de mem. guardado no p:\t\t%p", p);
+    
+    printf("\nEndereço de mem. da variável p:\t\t%p", &p);
+    
+    printf("\n\n\n");
+    return 0;
+}
