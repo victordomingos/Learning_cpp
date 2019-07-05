291ba5403f5580ccad9c0858211ff0afffb49719
Victor Domingos <victordomingos@users.noreply.github.com>
2019-07-05 16:16:19 +0100

    Update 02_dynamic_arrays.c

--- Data_Structures/02_dynamic_arrays.c
+++ Data_Structures/02_dynamic_arrays.c
@@ -10,15 +10,15 @@ int main()
     lista = (int *) malloc(sizeof(int) * tamanho_da_lista);
     cursor = lista;
 
-    for(int i=0; i<tamanho_da_lista;i++)
+    for(int i=0; i<tamanho_da_lista; i++)
     {
         *cursor = i * 2;
         cursor++;
     }
 
-    printf("Access the array elements using a cursor poiunter variable:\n");
+    printf("Access the array elements using a cursor pointer variable:\n");
     cursor = lista;
-    for(int i=0; i<tamanho_da_lista;i++)
+    for(int i=0; i<tamanho_da_lista; i++)
     {
         printf("%d: %d\n", i, *cursor);
         cursor++;
@@ -27,9 +27,9 @@ int main()
 
     printf("Accessing the array elements using square brackets:\n");
     cursor = lista;
-    for(int i=0; i<tamanho_da_lista;i++)
+    for(int i=0; i<tamanho_da_lista; i++)
     {
         printf("%d: %d\n", i, cursor[i]);
     }
-
+    return 0;
 }
