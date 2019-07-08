508a43ad39fd1cfa672dcf67b39454dd45f2e952
Victor Domingos <editor.arcosonline@gmail.com>
2019-07-08 08:18:50 +0100

    playing with casts to shut up gcc's pedantic

--- Data_Structures/01_variables_and_pointers.c
+++ Data_Structures/01_variables_and_pointers.c
@@ -8,14 +8,14 @@ int main ()
     p = &x;
 
     printf("\nValor de X: \t\t\t\t%d", x);
-    printf("\nEndereço de mem. da var. X: \t\t%p", p);
+    printf("\nEndereço de mem. da var. X: \t\t%p", (void *)p);
     
     printf("\nEndereço de mem. da variável apontada:\t%d", *p);
-    printf("\nEndereço de mem. guardado no p:\t\t%p", p);
+    printf("\nEndereço de mem. guardado no p:\t\t%p", (void *)p);
     
-    printf("\nEndereço de mem. da variável p:\t\t%p", &p);
+    printf("\nEndereço de mem. da variável p:\t\t%d", (unsigned int)&p);
 
-    printf("\nPergunta: que valor é este? :\t\t%p", &*p);
+    printf("\nPergunta: que valor é este? :\t\t%p", (void *)&*p);
     
     printf("\n\n\n");
     return 0;
