41c7021b943efecd0e5740378721c0c9615a6d82
Victor Domingos <editor.arcosonline@gmail.com>
2019-07-10 23:33:13 +0100

    final cleanup on 03_linked_lists.c

--- Data_Structures/03_linked_lists.c
+++ Data_Structures/03_linked_lists.c
@@ -23,12 +23,13 @@ node createNode(int value)
     return newNode;
 }
 
-void displayLinkedList(node linked_list)
+
+void displayLinkedList(node *linked_list)
 {    
     int usedMemory = 0;
-    int unitSize = sizeof(*linked_list);
+    int unitSize = sizeof(**linked_list);
 
-    node cursor = linked_list;
+    node cursor = *linked_list;
     while (cursor != NULL)
     {
         printf("%d  ", cursor->data);
@@ -36,40 +37,38 @@ void displayLinkedList(node linked_list)
         usedMemory += unitSize;
     }
     printf("\n==== Using %d bytes ====\n\n", usedMemory);
-    
 }
 
 
-node insertAtHead(node head, int value)
+int insertAtHead(node *head, int value)
 {
     node newHead = createNode(value);
     if(newHead != NULL)
     {
-        newHead->next = head;
-        head = newHead;
-        return head;    
+        newHead->next = *head;
+        *head = newHead;
+        return 0;    
     }
-    else return NULL;
+    else return 1;
 }
 
 
-node insertAtTail(node head, node tail, int value)
+int insertAtTail(node *head, node *tail, int value)
 {
     node newTail = createNode(value);
     if(newTail != NULL)
     {
-        tail->next = newTail;
-        tail = newTail;
-        return tail;
+        (*tail)->next = newTail;
+        (*tail) = newTail;
+        return 0;
     }
-    else return NULL;
+    else return 1;
 }
 
 
-
-node removeFirstOccurrence(node head, int value )
+int removeFirstOccurrence(node *head, int value )
 {
-    node cursor = head;
+    node cursor = *head;
 
     while (cursor->next->data != value && cursor->next->next != NULL)
         cursor = cursor->next;
@@ -80,9 +79,66 @@ node removeFirstOccurrence(node head, int value )
         cursor->next = cursor->next->next;
         free(p);
     }
-    return head;
+    return 0;
 }
 
+
+int insertInAscendingOrder(node *head, node *tail, int value)
+{
+    node cursor;
+    node newBodyNode = createNode(value);
+    for(cursor = *head; cursor->next->data < newBodyNode->data; cursor=cursor->next)
+        { } // Empty loop, just to set the cursor pointer to the intended position. 
+
+    newBodyNode->next = cursor->next;
+    cursor->next = newBodyNode;
+    return 0;
+}
+
+
+int removeFirstElement(node *head, node *tail)
+{
+    if (*head==*tail) // Is it a single element list?
+    {
+        free(*head);
+        *head=NULL;
+        *tail=NULL;
+        return 1;
+    }
+    else
+    {
+        node p = (*head)->next;
+        free(*head);
+        *head = p;
+        return 0;
+    }
+}
+
+
+int removeLastElement(node *head, node *tail)
+{
+    node cursor;
+
+    if (*head==*tail) // Is it a single element list?
+    {
+        free(*head);
+        head=NULL;
+        tail=NULL;
+        return 1;
+    }
+    else
+    {
+        cursor = *head;
+
+        while (cursor->next->next != NULL)
+            cursor = cursor->next;
+        
+        free(*tail);
+        cursor->next = NULL;
+        *tail = cursor;
+        return 0;
+    }
+}
 
 
 int main()
@@ -90,82 +146,37 @@ int main()
     printf("\nCreating a single element linked list.\n");
     node head = createNode(10);
     node tail = head;
-    node cursor;
+    displayLinkedList(&head);
 
-    
-    displayLinkedList(head);
 
     printf("\nInserting 10 elements at head.\n");
-    for(int i = 0; i < 10; i++)
-    {
-        node p = insertAtHead(head, 8);
-        if (p != NULL)
-            head = p;
-    }
-    displayLinkedList(head);
+    for(int i = 0; i < 10; i++)  { insertAtHead(&head, 8); }
+    displayLinkedList(&head);
 
 
     printf("\nInserting one at tail.\n");
-    node p = insertAtTail(head, tail, 15);
-    if (p != NULL)
-        tail = p;
-   
-    displayLinkedList(head);
+    insertAtTail(&head, &tail, 15);
+    displayLinkedList(&head);
     
     
     printf("\nInserting one inside body, in ascending order.\n");
-    node newBodyNode = createNode(12);
-    for(cursor = head; cursor->next->data < newBodyNode->data; cursor=cursor->next)
-        { } // Empty loop, just to set the cursor pointer to the intended position. 
-
-    newBodyNode->next = cursor->next;
-    cursor->next = newBodyNode;
-    displayLinkedList(head);
+    insertInAscendingOrder(&head, &tail, 12);
+    displayLinkedList(&head);
 
 
     printf("\nRemoving one from head.\n");
-    if (head==tail) // Is it a single element list?
-    {
-        free(head);
-        head=NULL;
-        tail=NULL;
-    }
-    else
-    {
-        node p = head->next;
-        free(head);
-        head = p;
-    }
-    displayLinkedList(head);
-
+    removeFirstElement(&head, &tail);
+    displayLinkedList(&head);
 
 
     printf("\nRemoving one from tail.\n");
-    if (head==tail) // Is it a single element list?
-    {
-        free(head);
-        head=NULL;
-        tail=NULL;
-    }
-    else
-    {
-        cursor = head;
-
-        while (cursor->next->next != NULL)
-            cursor = cursor->next;
-        
-        free(tail);
-        cursor->next = NULL;
-        tail = cursor;
-    }
-    displayLinkedList(head);
+    removeLastElement(&head, &tail);
+    displayLinkedList(&head);
 
 
-
-    //Remove from body
     printf("\nRemoving one from body where value == 10.\n");
-    head = removeFirstOccurrence(head, 10);
-    displayLinkedList(head);
+    removeFirstOccurrence(&head, 10);
+    displayLinkedList(&head);
 
     return 0;
 }
